5c2abd5506d09608776131bda448f8244b6a1068
Yutaka <y.tajika529@gmail.com>
2022-04-18 22:39:30 +0900

    modified: install danger-swift with brew

--- DevTools/Package.swift
+++ DevTools/Package.swift
@@ -11,14 +11,12 @@ let package = Package(
     ],
     dependencies: [
         .package(path: ".."),
-        .package(name: "danger-swift", url: "https://github.com/danger/swift.git", from: "3.0.0"),
         .package(url: "https://github.com/peripheryapp/periphery", branch: "master")
     ],
     targets: [
         .target(name: "DangerDependencies",
                 dependencies: [
-                    "DangerSwiftPeriphery",
-                    .product(name: "Danger", package: "danger-swift")
+                    "DangerSwiftPeriphery"
                 ])
     ]
 )
