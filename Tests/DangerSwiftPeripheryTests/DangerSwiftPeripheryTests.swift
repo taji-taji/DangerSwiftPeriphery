750b97957394dbb86defcbcba784b0244f1ca01d
Yutaka <y.tajika529@gmail.com>
2022-07-24 15:54:03 +0900

    test: add test for scan error occurred while parsing result

--- Tests/DangerSwiftPeripheryTests/DangerSwiftPeripheryTests.swift
+++ Tests/DangerSwiftPeripheryTests/DangerSwiftPeripheryTests.swift
@@ -36,12 +36,41 @@ final class DangerSwiftPeripheryTests: XCTestCase {
     }
     
     func testScanErrorOccurredWhileParsingResult() throws {
-        
+        let scanExecutor = PeripheryScanExecutableMock()
+        scanExecutor.executeHandler = {
+            "test"
+        }
+        let outputParser = CheckstyleOutputParsableMock(projectRootPath: DefaultCurrentPathProvider().currentPath)
+        outputParser.parseHandler = { _ in
+            throw TestError.parseError
+        }
+        let diffProvider = PullRequestDiffProvidableMock()
+        diffProvider.diffHandler = { _ in
+            .success(.modified(hunks: []))
+        }
+        let result = DangerPeriphery.scan(scanExecutor: scanExecutor,
+                                          outputParser: outputParser,
+                                          diffProvider: diffProvider)
+        switch result {
+        case .success:
+            XCTFail("Unexpected success")
+        case .failure(let error as TestError):
+            switch error {
+            case .parseError:
+                // noop
+                break
+            default:
+                XCTFail("Unexpected error")
+            }
+        default:
+            XCTFail("Unexpected result")
+        }
     }
 }
 
 private extension DangerSwiftPeripheryTests {
     enum TestError: Error {
-        case scanError(message: String)
+        case scanError
+        case parseError
     }
 }
