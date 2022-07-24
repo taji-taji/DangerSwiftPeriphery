c47bb6099e3f6dbc60eb3d01a3c08f6432d0e5dd
Yutaka <y.tajika529@gmail.com>
2022-07-24 14:08:55 +0900

    added: general PullRequestDiffProvidable mock object

---
+++ Tests/DangerSwiftPeripheryTests/Mocks/PullRequestDiffProvidableMock.swift
@@ -0,0 +1,18 @@
+//
+// Created by 多鹿豊 on 2022/07/24.
+//
+
+import Foundation
+import Danger
+@testable import DangerSwiftPeriphery
+
+final class PullRequestDiffProvidableMock: PullRequestDiffProvidable {
+    var diffHandler: ((String) -> Result<FileDiff.Changes, Error>)?
+
+    func diff(forFile: String) -> Result<FileDiff.Changes, Error> {
+        guard let handler = diffHandler else {
+            fatalError("diffHandler is nil.")
+        }
+        return handler(forFile)
+    }
+}
