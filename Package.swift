3e13afe02cafe929c639d6340e549cd493e9b4df
Yutaka <y.tajika529@gmail.com>
2022-07-25 22:27:30 +0900

    use swift5.6 toolchain

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -11,13 +11,13 @@ let package = Package(
             targets: ["DangerSwiftPeriphery"]),
     ],
     dependencies: [
-        .package(name: "danger-swift", url: "https://github.com/danger/swift.git", from: "3.0.0")
+        .package(url: "https://github.com/danger/swift.git", from: "3.0.0")
     ],
     targets: [
         .target(
             name: "DangerSwiftPeriphery",
             dependencies: [
-                .product(name: "Danger", package: "danger-swift")
+                .product(name: "Danger", package: "swift")
             ]),
         .testTarget(
             name: "DangerSwiftPeripheryTests",
