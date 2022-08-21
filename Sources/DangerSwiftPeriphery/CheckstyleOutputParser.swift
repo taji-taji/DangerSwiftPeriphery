8518db4dcaf83e481115e9104f843f4b38389f7b
Yutaka <y.tajika529@gmail.com>
2022-08-21 16:00:39 +0900

    Revert "modified: use XMLParser"
    This reverts commit e6696a42653687cbe15319647cbe467ffb1e1499.

--- Sources/DangerSwiftPeriphery/CheckstyleOutputParser.swift
+++ Sources/DangerSwiftPeriphery/CheckstyleOutputParser.swift
@@ -6,9 +6,6 @@
 //
 
 import Foundation
-#if canImport(FoundationXML)
-import FoundationXML
-#endif
 
 protocol CheckstyleOutputParsable {
     func parse(xml: String) throws -> [Violation]
@@ -22,61 +19,23 @@ struct CheckstyleOutputParser: CheckstyleOutputParsable {
     }
 
     func parse(xml: String) throws -> [Violation] {
-        let parser = XMLParser(data: xml.data(using: .utf8)!)
-        let delegate = CheckstyleOutputParserDelegate(projectRootPath: projectRootPath)
-        parser.delegate = delegate
-        parser.parse()
-
-        if !delegate.hasCheckstyleElement {
+        let xmlDocument = try XMLDocument(xmlString: xml, options: [])
+        guard let rootElement = xmlDocument.rootElement(),
+              rootElement.name == "checkstyle" else {
             throw Error.invalidCheckstyleXML
         }
-        return delegate.violations
-    }
-}
-
-final class CheckstyleOutputParserDelegate: NSObject, XMLParserDelegate {
-    private let projectRootPath: String
-    private var filePath: String?
-    private var line: Int?
-    private var message: String?
-    private(set) var violations: [Violation] = []
-    private(set) var hasCheckstyleElement = false
-
-    init(projectRootPath: String) {
-        self.projectRootPath = projectRootPath
-    }
-
-    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
-        switch elementName {
-        case "checkstyle":
-            hasCheckstyleElement = true
-        case "file":
-            filePath = attributeDict["name"]?
-                    .deletingPrefix(projectRootPath)
-                    .deletingPrefix("/")
-        case "error":
-            line = Int(attributeDict["line"] ?? "")
-            message = attributeDict["message"]
-        default:
-            break
-        }
-    }
-
-    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
-        switch elementName {
-        case "file":
-            filePath = nil
-        case "error":
-            if let filePath = filePath,
-               let line = line,
-               let message = message {
-                violations.append(Violation(filePath: filePath, line: line, message: message))
-            }
-            line = nil
-            message = nil
-        default:
-            break
-        }
+        let fileElements = (try? rootElement.nodes(forXPath: "file") as? [XMLElement]) ?? []
+        return fileElements.reduce([], { warnings, fileElement -> [Violation] in
+            guard let location = fileElement.attribute(forName: "name")?.stringValue else { return warnings }
+            let filePath = location.deletingPrefix(projectRootPath).deletingPrefix("/")
+            let errorElements = (try? fileElement.nodes(forXPath: "error") as? [XMLElement]) ?? []
+            return warnings + errorElements.compactMap({ errorElement -> Violation? in
+                guard let lineString = errorElement.attribute(forName: "line")?.stringValue,
+                      let line = Int(lineString),
+                      let message = errorElement.attribute(forName: "message")?.stringValue else { return nil }
+                return Violation(filePath: filePath, line: line, message: message)
+            })
+        })
     }
 }
 
